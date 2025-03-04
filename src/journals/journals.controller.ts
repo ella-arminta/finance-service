60e6fa272523f7c02865c71fdf4f955fa33a1dd5
ella <brigittaella15@gmail.com>
2025-03-04 10:51:26 +0700

    done journal

--- src/journals/journals.controller.ts
+++ src/journals/journals.controller.ts
@@ -1,6 +1,6 @@
 import { Controller } from '@nestjs/common';
 import { JournalsService } from './journals.service';
-import { MessagePattern } from '@nestjs/microservices';
+import { MessagePattern, Payload } from '@nestjs/microservices';
 import { Describe } from 'src/decorator/describe.decorator';
 import { ResponseDto } from 'src/common/response.dto';
 
@@ -15,11 +15,50 @@ export class JournalsController {
     description: 'Get Journals',
     fe: [
       'finance/journal:open',
-      'finance/journal:detail'
     ]
   })
-  async get() {
-    const data = await this.journalsService.getReport();
-    return ResponseDto.success('Data Found!', data, 200);
+  async get(@Payload() data: any) {
+    const filters = data.body;
+    // TODOELLA company_id  / store_id auth
+
+    // Change Filter Date Format
+    if (filters.dateStart != null) {
+      filters.dateStart = new Date(filters.dateStart);
+    }
+    if (filters.dateEnd != null) {
+      var endDate = new Date(filters.dateEnd);
+      endDate.setHours(23, 59, 59, 0); // Sets time to 23:59:59.000
+      filters.dateEnd = endDate;
+    }
+
+    const result = await this.journalsService.getJournals(filters);
+    return ResponseDto.success('Data Found!', result, 200);
+  }
+
+  @MessagePattern({ cmd: 'get:journal/*' })
+  @Describe({
+    description: 'Get Journals By ID',
+    fe: [
+      'finance/journal/detail:open',
+      'finance/journal:detail',
+    ]
+  })
+  async getByID(@Payload() data: any) {
+    const filters = data.body;
+    const id = data.params.id;
+    // TODOELLA company_id  / store_id auth
+
+    // Change Filter Date Format
+    if (filters.dateStart != null) {
+      filters.dateStart = new Date(filters.dateStart);
+    }
+    if (filters.dateEnd != null) {
+      var endDate = new Date(filters.dateEnd);
+      endDate.setHours(23, 59, 59, 0); // Sets time to 23:59:59.000
+      filters.dateEnd = endDate;
+    }
+
+    const result = await this.journalsService.getJournalsByID(id,filters);
+    return ResponseDto.success('Data Found!', result, 200);
   }
 }
--- src/journals/journals.service.ts
+++ src/journals/journals.service.ts
@@ -12,32 +12,132 @@ export class JournalsService extends BaseService<Journals> {
         super('journals', db, relations, true);
     }
 
-    async getReport() {
-        return [
-            {
-                id: 1,
-                name: 'Sales Journal',
-                code: 'S',
-                debit: 0,
-                credit: 2,
-                balance: 1
-            },
-            {
-                id: 2,
-                name: 'Sales Journal',
-                code: 'S',
-                debit: 1,
-                credit: 1,
-                balance: 1
-            },
-            {
-                id: 3,
-                name: 'Purchase Journal',
-                code: 'P',
-                debit: 2,
-                credit: 2,
-                balance: 2
-            }
-        ]
+    async getJournals(filters: any): Promise<any> {
+        let conditions: string[] = [];
+        let params: any[] = [];
+    
+        if (filters.dateStart) {
+            params.push(filters.dateStart);
+            conditions.push(`trans_date >= $${params.length}`);
+        }
+        if (filters.dateEnd) {
+            params.push(filters.dateEnd);
+            conditions.push(`trans_date <= $${params.length}`);
+        }
+    
+        let whereClause = conditions.length > 0 ? `WHERE ${conditions.join(" AND ")}` : "";
+    
+        const appendConditions = (baseWhere: string) => {
+            return conditions.length > 0 ? `${baseWhere ? baseWhere + " AND " : "WHERE "} ${conditions.join(" AND ")}` : baseWhere;
+        };
+    
+        let query = `
+            SELECT 
+                1 as id,
+                'Jurnal Umum' AS name,
+                SUM(CASE WHEN amount > 0 THEN amount ELSE 0 END) AS debit,
+                SUM(CASE WHEN amount < 0 THEN amount ELSE 0 END) AS credit,
+                SUM(amount) AS balance
+            FROM "Report_Journals"
+            ${whereClause}
+    
+            UNION ALL
+    
+            SELECT 
+                2 as id,
+                'Penjualan' AS name,
+                SUM(CASE WHEN amount > 0 THEN amount ELSE 0 END) AS debit,
+                SUM(CASE WHEN amount < 0 THEN amount ELSE 0 END) AS credit,
+                SUM(amount) AS balance
+            FROM "Report_Journals"
+            ${appendConditions("WHERE trans_type_id IN (3,7)")}
+    
+            UNION ALL
+    
+            SELECT 
+                3 as id,
+                'Pembelian' AS name,
+                SUM(CASE WHEN amount > 0 THEN amount ELSE 0 END) AS debit,
+                SUM(CASE WHEN amount < 0 THEN amount ELSE 0 END) AS credit,
+                SUM(amount) AS balance
+            FROM "Report_Journals"
+            ${appendConditions("WHERE trans_type_id IN (4, 5, 8)")}
+    
+            UNION ALL
+    
+            SELECT 
+                4 as id,
+                'Penerimaan Kas' AS name,
+                SUM(CASE WHEN amount > 0 THEN amount ELSE 0 END) AS debit,
+                SUM(CASE WHEN amount < 0 THEN amount ELSE 0 END) AS credit,
+                SUM(amount) AS balance
+            FROM "Report_Journals"
+            ${appendConditions("WHERE trans_type_id IN (3, 7, 2)")}
+    
+            UNION ALL
+    
+            SELECT 
+                5 as id,
+                'Pengeluaran & COGS' AS name,
+                SUM(CASE WHEN amount > 0 THEN amount ELSE 0 END) AS debit,
+                SUM(CASE WHEN amount < 0 THEN amount ELSE 0 END) AS credit,
+                SUM(amount) AS balance
+            FROM "Report_Journals"
+            ${appendConditions("WHERE trans_type_id IN (1, 4, 5, 8)")}`;
+    
+        const results = await this.db.$queryRawUnsafe(query, ...params);
+        return results;
     }
+
+    async getJournalsByID(id: number, filters: any) {
+        let conditions: string[] = [];
+        let params: any[] = [];
+    
+        if (filters.dateStart) {
+            params.push(filters.dateStart);
+            conditions.push(`rj.trans_date >= $${params.length}`);
+        }
+        if (filters.dateEnd) {
+            params.push(filters.dateEnd);
+            conditions.push(`rj.trans_date <= $${params.length}`);
+        }
+    
+        console.log(filters, id);
+        // Handle ID-based conditions
+        switch (Number(id)) {
+            case 2:
+                conditions.push(`rj.trans_type_id IN (3,7)`);
+                break;
+            case 3:
+                conditions.push(`rj.trans_type_id IN (4, 5, 8)`);
+                break;
+            case 4:
+                console.log('masuk 4');
+                conditions.push(`rj.trans_type_id IN (3, 7, 2)`);
+                break;
+            case 5:
+                conditions.push(`rj.trans_type_id IN (1, 4, 5, 8)`);
+                break;
+        }
+        console.log(conditions);
+    
+        let whereClause = conditions.length > 0 ? `WHERE ${conditions.join(" AND ")}` : "";
+    
+        let query = `
+            SELECT
+                rj.trans_date as date, 
+                rj.code as code,
+                a.name as account,
+                tt.name as label,
+                CASE WHEN rj.amount > 0 THEN rj.amount ELSE 0 END AS debit,
+                CASE WHEN rj.amount < 0 THEN rj.amount ELSE 0 END AS credit
+            FROM "Report_Journals" rj
+            JOIN "Accounts" a ON rj.account_id = a.id
+            JOIN "Trans_Type" tt ON rj.trans_type_id = tt.id
+            ${whereClause};
+        `;
+    
+        return await this.db.$queryRawUnsafe(query, ...params);
+    }
+    
 }
