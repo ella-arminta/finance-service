ca51832ef5ae3910d45d8d5fd3e3ec193999f87b
Ella Arminta <85121822+ella-arminta@users.noreply.github.com>
2025-06-03 15:02:38 +0700

    fix: gold report

--- src/task-schedule/task-schedule.service.ts
+++ src/task-schedule/task-schedule.service.ts
@@ -12,7 +12,7 @@ export class TaskScheduleService {
         private readonly db: DatabaseService,
     ) { }
 
-    @Cron('*/10 * * * *', {
+    @Cron('0 11 * * *', {
         timeZone: 'Asia/Jakarta',
     })
     async handleCron() {
@@ -26,7 +26,8 @@ export class TaskScheduleService {
 
     async scrapeDataGold(): Promise<any> {
         console.log('scraping gold value...');
-        const url = 'https://www.indogold.id/harga-emas-hari-ini';
+        // const url = 'https://www.indogold.id/harga-emas-hari-ini';
+        const url = 'https://www.anekalogam.co.id/id';
 
         try {
             const response = await firstValueFrom(
@@ -46,39 +47,33 @@ export class TaskScheduleService {
             const $ = cheerio.load(html);
             const harga: Record<string, string> = {};
 
-            $('.rectangle-card').each((_, element) => {
-                const produk = $(element).find('.subtitle').text().trim();
-                const tabel = $(element).find('table');
-        
-                // Cek apakah produk adalah Emas 99.5%
-                if (produk.includes('Emas 99.5%')) {
-                tabel.find('tbody tr').each((_, row) => {
-                    const pecahan = $(row).find('td').first().text().trim();
-                    let hargaBeli = $(row).find('td').eq(1).text().trim();
-                    let hargaJual = $(row).find('td').eq(2).text().trim();
-        
-                    // Cek jika pecahan adalah 1 gram
-                    if (pecahan === '1.0 Gram') {
-                        hargaBeli = hargaBeli.replace('Rp. ', '').replace(/,/g, '');
-                        hargaJual = hargaJual.replace('Rp. ', '').replace(/,/g, '');
+            // Mencari tabel harga emas LM
+            $('table.lm-table tbody tr').each((_, row) => {
+                const gramText = $(row).find('td').first().text().trim().toLowerCase();
 
+                if (gramText.includes('1gram')) {
+                    const hargaJual = $(row)
+                        .find('td')
+                        .eq(1)
+                        .text()
+                        .replace(/[^\d]/g, '');
+                    const hargaBeli = $(row)
+                        .find('td')
+                        .eq(2)
+                        .text()
+                        .replace(/[^\d]/g, '');
 
-        
-                        console.log('hargaBeli', hargaBeli);
-                        console.log('hargaJual', hargaJual);
-                        harga.sellPrice = hargaBeli;
-                        harga.buyPrice = hargaJual;
-                    }
-                });
+                    harga.hargaJual = parseInt(hargaBeli).toLocaleString('id-ID');
+                    harga.hargaBeli = parseInt(hargaJual).toLocaleString('id-ID');
                 }
             });
 
-            if (!harga.sellPrice || !harga.buyPrice) {
-                throw new Error('Failed to extract Harga Jual or Harga Beli.');
+            if (!harga.hargaBeli || !harga.hargaJual) {
+                throw new Error('Gagal mengambil harga untuk 1 gram Emas LM.');
             }
 
-            const buyPriceParsed = parseInt(harga.buyPrice.replace(/Rp|\./g, ""), 10);
-            const sellPriceParsed = parseInt(harga.sellPrice.replace(/Rp|\./g, ""), 10);
+            const buyPriceParsed = parseInt(harga.hargaBeli.replace(/Rp|\./g, ""), 10);
+            const sellPriceParsed = parseInt(harga.hargaJual.replace(/Rp|\./g, ""), 10);
             // Simpan ke database
             const newdata = await this.db.goldPrice.create({
                 data: {
@@ -92,60 +87,6 @@ export class TaskScheduleService {
             throw new Error(`Error fetching data: ${error.message}`);
         }
     }
-
-    // async scrapeData(): Promise<any> {
-    //     const url = 'https://www.indogold.id/harga-emas-hari-ini';
-    //     try {
-    //       const response = await axios.get(url, {
-    //         headers: {
-    //           'User-Agent':
-    //             'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/113.0.0.0 Safari/537.36',
-    //         },
-    //       });
-    
-    //       if (response.status !== 200) {
-    //         throw new Error(Failed to fetch data from ${url});
-    //       }
-    
-    //       const html = response.data;
-    //       const $ = cheerio.load(html);
-    //       const harga: Record<string, string> = {};
-    
-    //       // Menyaring harga hanya untuk produk "Emas 99.5%" untuk 1 gram
-    //       $('.rectangle-card').each((_, element) => {
-    //         const produk = $(element).find('.subtitle').text().trim();
-    //         const tabel = $(element).find('table');
-    
-    //         // Cek apakah produk adalah Emas 99.5%
-    //         if (produk.includes('Emas 99.5%')) {
-    //           tabel.find('tbody tr').each((_, row) => {
-    //             const pecahan = $(row).find('td').first().text().trim();
-    //             let hargaBeli = $(row).find('td').eq(1).text().trim();
-    //             let hargaJual = $(row).find('td').eq(2).text().trim();
-    
-    //             // Cek jika pecahan adalah 1 gram
-    //             if (pecahan === '1.0 Gram') {
-    //               hargaBeli = hargaBeli.replace('Rp. ', '').replace(',', '.');
-    //               hargaJual = hargaJual.replace('Rp. ', '').replace(',', '.');
-    
-    //               harga.hargaBeli = hargaBeli;
-    //               harga.hargaJual = hargaJual;
-    //             }
-    //           });
-    //         }
-    //       });
-    
-    //       if (!harga.hargaBeli || !harga.hargaJual) {
-    //         throw new Error('Failed to extract prices for 1 gram of Emas 99.5%.');
-    //       }
-    
-    //       console.log(harga);
-    //       return harga;
-    //     } catch (error) {
-    //       console.error('Error scraping data:', error);
-    //       throw error;
-    //     }
-    // }
 
     async getGoldPrice(filter: any = {}) {
         if (filter.end_date) {
