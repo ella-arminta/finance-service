ddfaa5f4fac8877fcd7bc5f2b6441c93f4e33b02
Ella Arminta <85121822+ella-arminta@users.noreply.github.com>
2025-06-17 21:26:54 +0700

    fix: seeder dideploy

---
+++ runSeeder2.ts
@@ -0,0 +1,9 @@
+// prisma/runSeeder.ts
+// C:\xampp\htdocs\skripsi\finance-service\prisma\seeder\account_type.seeder.ts
+import './prisma/seeder/account_type.seeder';
+import './prisma/seeder/action_account_settings.seeder';
+import './prisma/seeder/recurring_period.seeder';
+import './prisma/seeder/stock_source.seeder';
+import './prisma/seeder/trans_type.seeder';
+// Tambahkan seeder lain sesuai kebutuhan
+console.log('✅ Seeder selesai.');
